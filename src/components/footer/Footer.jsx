a102a014ab3b42e52fd76c655487d92cf928c5be
Aditi Ray <imakeothersmile@gmail.com>
2023-04-05 23:15:30 +0530

    Updated Changes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,5 @@
 import "./footer.scss";
-import Linkedin from "@iconscout/react-unicons/icons/uil-linkedin";
-import Github from "@iconscout/react-unicons/icons/uil-github";
-import Facebook from "@iconscout/react-unicons/icons/uil-facebook";
-import Insta from "@iconscout/react-unicons/icons/uil-instagram";
+import {LinkedIn, GitHub, Facebook, Instagram } from "@material-ui/icons";
 
 export default function Footer() {
   return (
@@ -12,10 +9,10 @@ export default function Footer() {
         <span>This Website is made by Aditi Ray</span>
         <p>© March 2023</p>
         <div className="f-icons">
-        <a href="https://www.linkedin.com/in/thisisaditi/" target="_blank"><Linkedin color="white" size="2rem"/></a>
-        <a href="https://github.com/iamaditiray" target="_blank"><Github color="white" size="2rem"/></a>
-        <a href="https://www.facebook.com/aditiray2001/" target="_blank"><Facebook color="white" size="2rem"/></a>
-        <a href="https://www.instagram.com/amour_immacule/" target="_blank"><Insta color="white" size="2rem"/></a>
+        <a href="https://www.linkedin.com/in/thisisaditi/" target="_blank" rel="noreferrer"><LinkedIn color="white" size="2rem"/></a>
+        <a href="https://github.com/iamaditiray" target="_blank" rel="noreferrer"><GitHub color="white" size="2rem"/></a>
+        <a href="https://www.facebook.com/aditiray2001/" target="_blank" rel="noreferrer"><Facebook color="white" size="2rem"/></a>
+        <a href="https://www.instagram.com/amour_immacule/" target="_blank" rel="noreferrer"><Instagram color="white" size="2rem"/></a>
         </div>
       </div>
     </div>
